20531eba0bd803bd20f6a8a145a824fd3f27388f
Peter Fox <peter.fox@peterfox.me>
2016-03-09 19:18:03 +0000

    Fixes a minor issue with Auth class resolution breaking the authentication setup

--- src/Laravel/Http/Middleware/Authenticate.php
+++ src/Laravel/Http/Middleware/Authenticate.php
@@ -17,7 +17,7 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->guest()) {
+        if (\Auth::guard($guard)->guest()) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
